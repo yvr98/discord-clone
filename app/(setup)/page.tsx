f03a0c926bcf63247614283e7765c25926562c36
yvr98 <yudthesvar98@gmail.com>
2023-10-02 15:21:28 +0800

    Form set up

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import {redirect} from 'next/navigation'
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import {InitialModal} from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     
@@ -21,7 +22,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`);
     }
      
-    return <div>Create a Server </div>;
+    return <InitialModal/>;
     
 }
  
